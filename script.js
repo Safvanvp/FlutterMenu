18825376c157474ff14647eb43e1d6e119eaccd5
Safvan vp <167276814+Safvanvp@users.noreply.github.com>
2025-06-17 21:40:12 +0530

    s

---
+++ script.js
@@ -0,0 +1,6 @@
+// JS placeholder for future enhancements like filtering or dynamic pricing
+console.log("Welcome to Flutter Cafe!");
+document.querySelectorAll('.card').forEach((card, index) => {
+    card.style.animationDelay = `${index * 0.1}s`;
+});
+
